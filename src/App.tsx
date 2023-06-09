6074463991e37b2d3ed52af2a13987207640c024
Jandro <71239294+coolcoolcoolyo@users.noreply.github.com>
2023-06-09 00:44:13 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <div>test</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nav() {
+    
+    return (
+        <div>
+            I really need this to work
+        </div>
+    )
+}
+
+export default Nav
